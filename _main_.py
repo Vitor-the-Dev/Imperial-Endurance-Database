e80ef2ccd10f412c75b12282b56aecc79505976c
Vitor-the-Dev <vitorferthedev@gmail.com>
2023-06-06 21:32:44 -0300

    fix: Temporarily removed input for _main_.py to use with pipelines

---
+++ _main_.py
@@ -0,0 +1,16 @@
+from find_stops import find_stops
+
+def main():
+    #distance = int(input('please inform the distance '))
+
+    find_stops(1000000)
+
+if __name__ == "__main__":
+    main()
+    
+    
+   
+    
+    
+
+
